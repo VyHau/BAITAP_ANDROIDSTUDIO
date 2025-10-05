16233514b824b0b47dd981525a5608db1e2aa4f0
VyHau <vyhau2005@gmail.com>
2025-10-05 12:45:55 +0700

    nhan thong tin tu claa login truyen toi va code lang nghe su kien cho button setting

--- BaiTap_ChuyenActivity/app/src/main/java/com/example/baitap_chuyenactivity/Profile.java
+++ BaiTap_ChuyenActivity/app/src/main/java/com/example/baitap_chuyenactivity/Profile.java
@@ -1,6 +1,10 @@
 package com.example.baitap_chuyenactivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
@@ -9,5 +13,21 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        Intent i = getIntent();
+        String username= i.getStringExtra("email");
+        String passWord = i.getStringExtra("pass");
+        TextView txt = findViewById(R.id.tvUsername_2311);
+        TextView email = findViewById(R.id.tvEmail_2311);
+        TextView pass = findViewById(R.id.tvPass_2311);
+        ImageButton btnSetting = findViewById(R.id.btnSetting_2311);
+        txt.setText(username);
+        email.setText(username);
+        pass.setText(passWord);
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
