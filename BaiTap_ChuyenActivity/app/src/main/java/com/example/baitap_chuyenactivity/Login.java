cde5e1c5e9c8a2783842405234f0d0ce0c59def0
VyHau <vyhau2005@gmail.com>
2025-10-05 12:30:26 +0700

    code lang nghe su kien cho button login

--- BaiTap_ChuyenActivity/app/src/main/java/com/example/baitap_chuyenactivity/Login.java
+++ BaiTap_ChuyenActivity/app/src/main/java/com/example/baitap_chuyenactivity/Login.java
@@ -1,6 +1,10 @@
 package com.example.baitap_chuyenactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
@@ -9,5 +13,20 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        EditText editText1 = findViewById(R.id.editUsername_2311);
+        EditText editText2 = findViewById(R.id.editPassword_2311);
+        Button btn = findViewById(R.id.btnLogin_2311);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(Login.this,Profile.class);
+                String username = editText1.getText().toString();
+                String pass = editText2.getText().toString();
+                i.putExtra("email",username);
+                i.putExtra("pass",pass);
+                startActivity(i);
+            }
+        });
     }
 }
