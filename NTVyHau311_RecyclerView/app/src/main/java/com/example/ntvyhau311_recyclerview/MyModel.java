ad1c2e03496892059e26854e13b12c0eb2146b3f
VyHau <vyhau2005@gmail.com>
2025-11-08 00:25:08 +0700

    Tạo model cho Dish

---
+++ NTVyHau311_RecyclerView/app/src/main/java/com/example/ntvyhau311_recyclerview/MyModel.java
@@ -0,0 +1,21 @@
+package com.example.ntvyhau311_recyclerview;
+
+public class MyModel {
+    private String foodName_311;
+    private String description_311;
+    private String price_311;
+    public MyModel(String foodName_311, String description_311, String price_311) {
+        this.foodName_311 = foodName_311;
+        this.description_311 = description_311;
+        this.price_311 = price_311;
+    }
+    public String getFoodName_311() {
+        return foodName_311;
+    }
+    public String getDescription_311() {
+        return description_311;
+    }
+    public String getPrice_311() {
+        return price_311;
+    }
+}
