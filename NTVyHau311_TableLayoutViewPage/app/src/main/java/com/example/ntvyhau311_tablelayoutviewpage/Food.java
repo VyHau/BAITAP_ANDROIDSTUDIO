24385ec6b7ed96d31694734f6f575eb330ac30f3
VyHau <vyhau2005@gmail.com>
2025-11-15 10:52:00 +0700

    Create Model food.java

---
+++ NTVyHau311_TableLayoutViewPage/app/src/main/java/com/example/ntvyhau311_tablelayoutviewpage/Food.java
@@ -0,0 +1,18 @@
+package com.example.ntvyhau311_tablelayoutviewpage;
+
+
+public class Food {
+    private int image_311;
+    private String name_311;
+    private String price_311;
+
+    public Food(int image, String name, String price) {
+        this.image_311 = image;
+        this.name_311 = name;
+        this.price_311 = price;
+    }
+
+    public int getImage() { return image_311; }
+    public String getName() { return name_311; }
+    public String getPrice() { return price_311; }
+}
